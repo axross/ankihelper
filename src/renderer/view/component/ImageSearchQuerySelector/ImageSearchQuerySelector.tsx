a93ffd1e061a7d635449ed23dfe73e1d803254ec
axross <me@axross.io>
2018-08-01 18:54:22 -0700

    Refactor <ImageSearchQuerySelector>

--- src/renderer/view/component/ImageSearchQuerySelector/ImageSearchQuerySelector.tsx
+++ src/renderer/view/component/ImageSearchQuerySelector/ImageSearchQuerySelector.tsx
@@ -5,46 +5,44 @@ import styled from '../../../core/styled-components';
 
 type Props = {
   text: string;
-  defaultValue?: string[];
-  onChange?: (selected: string[]) => void;
+  defaultValue: string[];
+  onChange: (selected: string[]) => void;
   className?: string;
 };
 
-const ImageSearchQuerySelector = ({ text, defaultValue = [], onChange, className }: Props) => {
-  return (
-    <Value initial={defaultValue} onChange={onChange}>
-      {({ set, value: selected }) => (
-        <Root className={className}>
-          {text.split(' ').map(word => (
-            <Word
-              intent={selected.indexOf(word) === -1 ? 'none' : 'primary'}
-              interactive
-              onClick={() => {
-                if (selected.indexOf(word) === -1) {
-                  set([...selected, word]);
-                } else {
-                  if (selected.length === 1) return;
+const ImageSearchQuerySelector = ({ text, defaultValue, onChange, className }: Props) => (
+  <Value initial={defaultValue} onChange={onChange}>
+    {({ set, value: selected }) => (
+      <Root className={className}>
+        {Array.from(new Set(text.split(' '))).map(word => (
+          <Word
+            intent={selected.indexOf(word) === -1 ? 'none' : 'primary'}
+            interactive
+            onClick={() => {
+              if (selected.indexOf(word) === -1) {
+                set([...selected, word]);
+              } else {
+                if (selected.length === 1) return;
 
-                  set(selected.filter(w => w !== word));
-                }
-              }}
-              key={word}
-            >
-              {word}
-            </Word>
-          ))}
-        </Root>
-      )}
-    </Value>
-  );
-};
+                set(selected.filter(w => w !== word));
+              }
+            }}
+            key={word}
+          >
+            {word}
+          </Word>
+        ))}
+      </Root>
+    )}
+  </Value>
+);
 
 const Root = styled.div`
   padding-right: -4px;
   padding-bottom: -4px;
 `;
 
-const Word = styled(Tag)`
+export const Word = styled(Tag)`
   margin-right: 4px;
   margin-bottom: 4px;
 `;
