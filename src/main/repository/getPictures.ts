2904441d274cd80f6000a8c11e40322d6ecc62df
axross <me@axross.io>
2018-08-01 18:54:57 -0700

    Improve image searching function

--- src/main/repository/getPictures.ts
+++ src/main/repository/getPictures.ts
@@ -1,29 +1,9 @@
-import * as compromise from 'compromise';
 import fetch from 'node-fetch';
 
 const getPictures = async (text: string, { bingImageSearchApiKey }: { bingImageSearchApiKey: string }) => {
-  const people = compromise(text)
-    .people()
-    .data()
-    .map((data: any) => data.normal);
-  const nouns = compromise(text)
-    .nouns()
-    .data()
-    .map((data: any) => data.normal)
-    .filter((word: string) => people.indexOf(word) !== 1);
-  const adjectives = compromise(text)
-    .adjectives()
-    .data()
-    .map((data: any) => data.normal);
-  const adverbs = compromise(text)
-    .adverbs()
-    .data()
-    .map((data: any) => data.normal);
-  const query = [...nouns, ...adjectives, ...adverbs].slice(0, 3).join(' ');
-
   const response = await fetch(
     `https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=${encodeURIComponent(
-      query
+      text
     )}&count=100&safeSearch=Moderate`,
     {
       headers: {
